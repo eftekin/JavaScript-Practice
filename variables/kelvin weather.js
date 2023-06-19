e5206ea3fa2ac55e4e5f30f121024d5144d1081b
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-06-19 23:44:29 +0300

    Create kelvin weather.js

---
+++ variables/kelvin weather.js
@@ -0,0 +1,15 @@
+// kelvin's constant
+let kelvin = 293
+// calculation of celsius
+let celsius = kelvin - 273
+// calculation of fahrenheit
+let fahrenheit = celsius * (9/5) + 32
+// round the decimal
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+// EXTRA (calculation of newton)
+let newton = celsius * (33/100)
+newton = Math.floor(newton)
+console.log(`The temperature is ${newton} degrees Newton.`)
