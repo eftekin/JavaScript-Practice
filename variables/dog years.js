d0d1c066c0ed0e986ca6ae43b4e94d6e5d1746e6
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-06-20 00:12:51 +0300

    Create dog years.js

---
+++ variables/dog years.js
@@ -0,0 +1,14 @@
+// my age as a constant
+const myAge = 20
+
+// dog age formula
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+
+let myAgeInDogYears = earlyYears + laterYears
+let myName = "Mustafa Eftekin".toLowerCase()
+
+// print the value
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
