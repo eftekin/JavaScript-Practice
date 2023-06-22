afdd9a554ab5b3f2f903ef7694f64e5faa9d22c1
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-06-22 13:16:23 +0300

    Create rock paper scissors.js

---
+++ functions/rock paper scissors.js
@@ -0,0 +1,67 @@
+const getUserChoice = (userInput) => {
+  var userInput = "rock";
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    userInput = userInput.toLowerCase();
+    return userInput;
+  } else {
+    console.log("Something went wrong.");
+  }
+};
+
+function getComputerChoice() {
+  randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "game was a tie.";
+  }
+
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "the computer won.";
+    } else {
+      return "the user won.";
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChocie === "scissors") {
+      return "the computer won.";
+    } else {
+      return "the user won.";
+    }
+  }
+
+  if (userChoice === "scissors ") {
+    if (computerChoice === "rock") {
+      return "the computer won.";
+    } else {
+      return "the user won.";
+    }
+  }
+}
+
+function playGame() {
+  const userChoice = getUserChoice();
+  const computerChoice = getComputerChoice();
+  console.log(`You threw: ${userChoice}`);
+  console.log(`The computer threw ${computerChoice}`);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
