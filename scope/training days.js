ee6be890389cddc8b6e7f71402892c484199b40b
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-06-23 14:20:11 +0300

    Create training days.js

---
+++ scope/training days.js
@@ -0,0 +1,38 @@
+const random = Math.floor(Math.random() * 3);
+
+const getRandEvent = () => {
+  if (random === 0) {
+    return "Marathon";
+  } else if (random === 1) {
+    return "Triathlon";
+  } else if (random === 2) {
+    return "Pentathlon";
+  }
+};
+
+const getTrainingDays = (event) => {
+  let days;
+  if (event === "Marathon") {
+    days = 50;
+  } else if (event === "Triathlon") {
+    days = 100;
+  } else if (event === "Pentathlon") {
+    days = 200;
+  }
+  return days;
+};
+
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+const name = "Nala";
+
+logEvent(name, event);
+logTime(name, days);
