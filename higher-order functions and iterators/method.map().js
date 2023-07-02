115ffef644cc04e2c812b6295145b9d63b3681b6
Mustafa Eftekin <m_eftekin@hotmail.com>
2023-07-03 02:54:55 +0300

    Create method.map().js

---
+++ higher-order functions and iterators/method.map().js
@@ -0,0 +1,13 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+const secretMessage = animals.map(animal => animal[0])
+console.log(secretMessage.join(""))
+
+////////////////////////////////////
+const bigNumbers = [100, 200, 300, 400, 500]
+
+const smallNumbers = bigNumbers.map(number =>{
+    return number/100
+})
+
+console.log(smallNumbers)
